536804e6acda27072f4ee1936748d0ed26826577
Vovilonn <vovadesign0792@gmail.com>
2020-11-19 20:22:01 +0200

    maked header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+    Button,
+    Box,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton
+                    edge="start"
+                    className={classes.menuButton}
+                    color="inherit"
+                    aria-label="menu"
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    News
+                </Typography>
+                <Box mr={3}>
+                    <Button color="inherit" variant="outlined">
+                        Log In
+                    </Button>
+                    <Button color="secondary" variant="contained">
+                        Sign In
+                    </Button>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
 }
 
 export default App;
